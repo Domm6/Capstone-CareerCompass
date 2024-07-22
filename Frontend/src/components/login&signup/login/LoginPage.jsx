9e0984218677ea16f17cc85d81eb10c18364ecbc
Dominic Parker <dominicparker@meta.com>
2024-07-22 09:55:16 -0700

    load state to login and signup

--- Frontend/src/components/login&signup/login/LoginPage.jsx
+++ Frontend/src/components/login&signup/login/LoginPage.jsx
@@ -3,12 +3,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../UserContext";
 import "./LoginPage.css";
 import config from "../../../../config";
+import { CircularProgress } from "@mui/material";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { updateUser } = useContext(UserContext);
   const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -16,6 +18,7 @@ const LoginPage = () => {
     event.preventDefault();
 
     try {
+      setLoading(true);
       const response = await fetch(`${config.apiBaseUrl}/users/login`, {
         method: "POST",
         headers: {
@@ -41,8 +44,10 @@ const LoginPage = () => {
       } else {
         setErrorMessage("Wrong email or password, please try again");
       }
+      setLoading(false);
     } catch (error) {
       setErrorMessage(`Login failed: ${error.message}`);
+      setLoading(false);
     }
   };
 
@@ -52,33 +57,39 @@ const LoginPage = () => {
         <h1>Welcome to CareerCompass!</h1>
       </div>
       <div className="login-form-container">
-        <form className="login-form" onSubmit={handleLogin}>
-          <h2>Login</h2>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              required
-            />
+        {loading ? (
+          <div className="loading-spinner">
+            <CircularProgress />
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-          <p>
-            New to the app? <Link to="/signup">Sign Up</Link>
-          </p>
-        </form>
+        ) : (
+          <form className="login-form" onSubmit={handleLogin}>
+            <h2>Login</h2>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+            <p>
+              New to the app? <Link to="/signup">Sign Up</Link>
+            </p>
+          </form>
+        )}
         {errorMessage && <div className="error-message">{errorMessage}</div>}
       </div>
     </>
--- Frontend/src/components/login&signup/signup/SignUpPage.jsx
+++ Frontend/src/components/login&signup/signup/SignUpPage.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./SignUpPage.css";
 import { UserContext } from "../../../UserContext.jsx";
 import config from "../../../../config.js";
+import { CircularProgress } from "@mui/material";
 
 const SignUpPage = () => {
   const [name, setName] = useState("");
@@ -10,12 +11,14 @@ const SignUpPage = () => {
   const [password, setPassword] = useState("");
   const [userRole, setUserRole] = useState("");
   const { updateUser } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
+      setLoading(true);
       // Make the signup API request
       const response = await fetch(`${config.apiBaseUrl}/users/signup`, {
         method: "POST",
@@ -50,9 +53,11 @@ const SignUpPage = () => {
         // Handle signup failure case
         alert("Signup failed");
       }
+      setLoading(false);
     } catch (error) {
       // Handle any network or API request errors
       alert("Signup failed: " + error);
+      setLoading(false);
     }
   };
 
@@ -62,55 +67,61 @@ const SignUpPage = () => {
         <h1>Welcome to CareerCompass!</h1>
       </div>
       <div className="signup-page-container">
-        <form className="signup-form" onSubmit={handleSubmit}>
-          <h2>Sign Up</h2>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-              required
-            />
+        {loading ? (
+          <div className="loading-spinner">
+            <CircularProgress />
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="user-role">User Role:</label>
-            <select
-              value={userRole}
-              onChange={(event) => setUserRole(event.target.value)}
-              required
-            >
-              <option value="">Select a Role</option>
-              <option value="mentor">Mentor</option>
-              <option value="mentee">Mentee</option>
-            </select>
-          </div>
-          <button type="submit">Sign Up</button>
-          <p>
-            Already have an account? <Link to="/login">Log In</Link>
-          </p>
-        </form>
+        ) : (
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <h2>Sign Up</h2>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="user-role">User Role:</label>
+              <select
+                value={userRole}
+                onChange={(event) => setUserRole(event.target.value)}
+                required
+              >
+                <option value="">Select a Role</option>
+                <option value="mentor">Mentor</option>
+                <option value="mentee">Mentee</option>
+              </select>
+            </div>
+            <button type="submit">Sign Up</button>
+            <p>
+              Already have an account? <Link to="/login">Log In</Link>
+            </p>
+          </form>
+        )}
       </div>
     </>
   );
